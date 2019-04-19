e79a67a9cb46d4686b908164d5b8b367d0f1e118
Nikolay Sinelnikov <nikola.sinelnik@gmail.com>
2019-04-19 11:45:16 +0300

    add gulp task scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,20 @@
-
 var gulp = require('gulp');
+var gulpIf = require('gulp-if');
+var scss = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
+/* SCSS */
+gulp.task('scss', function(){
+	return gulp.src('src/css/*.scss')
+	.pipe(gulpIf(isDevelopment, sourcemaps.init()))
+	.pipe(scss({outputStyle: 'expanded'}).on('error', scss.logError))
+	.pipe(autoprefixer({
+        browsers: ['last 15 versions'],
+        cascade: false
+    }))
+	.pipe(gulpIf(isDevelopment, sourcemaps.write()))
+	.pipe(gulp.dest('src/css'))
+});
